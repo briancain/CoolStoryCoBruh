caa10e97e94791030b30255fcca939e0c0dd4583
Preben Ingvaldsen <preben.ingvaldsen@gmail.com>
2016-02-13 08:49:26 -0800

    Add CoBruh prefab with basic movement
    Add a CoBruh prefab with a placeholder sprite and basic movement.
    Add a test scene. Move scenes into a new folder.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float playerSpeed;
+
+	private Rigidbody2D rb;
+
+	void Start() {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	// Moves the player based on input
+	private void Move() {
+		float xVel = playerSpeed * Input.GetAxisRaw ("Horizontal");
+		float yVel = playerSpeed * Input.GetAxisRaw ("Vertical");
+
+		rb.velocity = new Vector2 (xVel, yVel);
+	}
+}
