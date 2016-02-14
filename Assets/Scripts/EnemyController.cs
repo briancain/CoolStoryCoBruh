b00884107d5e87be00b954f5e9a3398908a6bea1
Preben Ingvaldsen <preben.ingvaldsen@gmail.com>
2016-02-13 17:01:11 -0800

    Add Stationary Enemy to game

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : StationaryEnemyController {
 
   public float enemySpeed;
   public GameObject[] waypoints;
@@ -10,45 +10,30 @@ public class EnemyController : MonoBehaviour {
   private int currWaypoint = 1;
   private bool forwardPath = true;
   private bool moving = true;
-  public bool alert = false;
 
   private float stopTimer = 0f;
-
-  private float losDistance = 15f;
-  private float losAngle = 120f;
-
-  private Vector2 facing;
-
-  private GameObject player;
 
   /////////////////////////////////
   /// Unity Methods
   /////////////////////////////////
 
 	void Start () {
-		rb = gameObject.GetComponent<Rigidbody2D> ();
-    player = GameObject.FindGameObjectWithTag (Tags.PLAYER);
+    base.Start ();
+    rb = gameObject.GetComponent<Rigidbody2D> ();
     facing = (waypoints [currWaypoint].transform.position - gameObject.transform.position).normalized;
 	}
 	
 	void Update () {
     if (!alert) {
       Move ();
-    } else {
-      facing = (player.transform.position - gameObject.transform.position).normalized;
     }
-    LineOfSight ();
+    base.Update ();
   }
 
 
   /////////////////////////////////
   /// Public Methods
   /////////////////////////////////
-
-  // Send this enemy into an alert state
-  public void Alert() {
-    alert = true;
-  }
 
   // End this enemy's alert status
   public void EndAlert() {
@@ -94,20 +79,6 @@ public class EnemyController : MonoBehaviour {
       print ("Should stop");
       moving = false;
       facing = wp.directionToLook;
-    }
-  }
-
-  // Look for the player
-  private void LineOfSight() {
-    bool closeEnough = Vector2.Distance (gameObject.transform.position, player.transform.position) <= losDistance;
-    Vector2 toPlayer = (player.transform.position - gameObject.transform.position).normalized;
-    float angleBetween = Vector2.Angle (facing, toPlayer);
-    if (closeEnough && angleBetween <= losAngle / 2.0f) {
-      // The player is within the guard's LOS, so make a raycast to ensure nothing is in the way
-      RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, toPlayer, losDistance, ~Layers.CreateLayerMask(Layers.IGNORE_RAYCAST));
-      if (hit != null && hit.collider.gameObject.tag == Tags.PLAYER) {
-        print ("ENEMY SAW PLAYER");
-      }
     }
   }
 }
---
+++ Assets/Scripts/StationaryEnemyController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StationaryEnemyController : MonoBehaviour {
+  protected bool alert = false;
+  public Vector2 defaultFacing;
+
+  private float losDistance = 15f;
+  private float losAngle = 120f;
+
+  protected Vector2 facing;
+
+  protected GameObject player;
+
+  /////////////////////////////////
+  /// Unity Methods
+  /////////////////////////////////
+
+  protected virtual void Start () {
+    player = GameObject.FindGameObjectWithTag (Tags.PLAYER);
+    facing = defaultFacing;
+  }
+
+  protected virtual void Update () {
+    if (alert) {
+      facing = (player.transform.position - gameObject.transform.position).normalized;
+    }
+    LineOfSight ();
+  }
+
+  // Send this enemy into an alert state
+  public void Alert() {
+    alert = true;
+  }
+
+  // End this enemy's alert status
+  public virtual void EndAlert() {
+    facing = defaultFacing;
+    alert = false;
+  }
+
+  // Look for the player
+  private void LineOfSight() {
+    bool closeEnough = Vector2.Distance (gameObject.transform.position, player.transform.position) <= losDistance;
+    Vector2 toPlayer = (player.transform.position - gameObject.transform.position).normalized;
+    float angleBetween = Vector2.Angle (facing, toPlayer);
+    if (closeEnough && angleBetween <= losAngle / 2.0f) {
+      // The player is within the guard's LOS, so make a raycast to ensure nothing is in the way
+      RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, toPlayer, losDistance, ~Layers.CreateLayerMask(Layers.IGNORE_RAYCAST));
+      if (hit != null && hit.collider.gameObject.tag == Tags.PLAYER) {
+        print ("ENEMY SAW PLAYER");
+      }
+    }
+  }
+}
