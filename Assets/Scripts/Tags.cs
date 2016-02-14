c2f284bd5d05cbc4f121967b7391873118511f12
Brian Cain <brianccain@gmail.com>
2016-02-14 14:30:45 -0800

    WIP for ui elements

--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -10,4 +10,5 @@ public class Tags : MonoBehaviour {
   public const string ELEVATOR = "Elevator";
   public const string KEY = "Key";
   public const string DOOR = "Door";
+  public const string SNAKE_BAR = "SnakeBar";
 }
