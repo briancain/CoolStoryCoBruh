dbbb05107a4f4b4316f51bc708b736b8d98c1d0d
Preben Ingvaldsen <preben.ingvaldsen@gmail.com>
2016-02-14 19:42:57 -0800

    Add start screen

---
+++ Assets/Scripts/StartScreenController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartScreenController : MonoBehaviour {
+  float timer = 0.0f;
+  void Update() {
+    timer += Time.deltaTime;
+    if (timer >= 171.0f || Input.GetKeyDown(KeyCode.Space)) {
+      SceneManager.LoadScene (1);
+    }
+  }
+}
