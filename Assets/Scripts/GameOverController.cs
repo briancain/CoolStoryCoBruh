315c45d5dc28d1eb21e801f7b1964c43574415e2
Preben Ingvaldsen <preben.ingvaldsen@gmail.com>
2016-02-14 19:50:17 -0800

    Add GameOver scene

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameOverController : MonoBehaviour {
+
+  void Update() {
+    if (Input.GetKeyDown (KeyCode.Space)) {
+      SceneManager.LoadScene (1);
+    }
+  }
+}
