7d731542660b6be151a895166f10ec5b6a69aeef
Brian Cain <brianccain@gmail.com>
2016-03-06 20:36:30 -0800

    Move scripts into Scripts folder, add GameWin music

---
+++ Assets/Scripts/GameWinController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameWinController : MonoBehaviour {
+
+  public AudioClip gameWin;
+  private AudioSource audio;
+
+  void Start() {
+    audio = GetComponent<AudioSource>();
+    audio.PlayOneShot(gameWin, 1.0f);
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if (Input.GetKeyDown (KeyCode.Space)) {
+      Application.Quit ();
+    }
+  }
+}
