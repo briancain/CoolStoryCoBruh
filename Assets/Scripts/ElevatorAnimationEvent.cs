601951bccc95c48dd70ff422b07d409b23a1ffb0
Preben Ingvaldsen <preben.ingvaldsen@gmail.com>
2016-03-06 21:22:01 -0800

    Add GameWin voiceclip

--- Assets/Scripts/ElevatorAnimationEvent.cs
+++ Assets/Scripts/ElevatorAnimationEvent.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class ElevatorAnimationEvent : MonoBehaviour {
 
   public GameObject gameManager;
+  public AudioClip ohKO;
 
   private GameManager gm;
+  private PlayerController pc;
+  private AudioSource audio;
 
   void Start() {
     gm = gameManager.GetComponent<GameManager> ();
+    audio = GetComponent<AudioSource> ();
   }
 
   void WinGame() {
     gm.WinGame ();
+  }
+
+  void PlayerSpeak() {
+    audio.PlayOneShot (ohKO);
   }
 }
